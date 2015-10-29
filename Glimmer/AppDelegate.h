12387e6ddecbff54fdd3a3fcc5d1fda291af9c38
MikeRiy <542540443@qq.com>
2015-10-29 21:08:50 +0800

    no message

---
+++ Glimmer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Glimmer
+//
+//  Created by MikeRiy on 15/10/29.
+//  Copyright © 2015年 MikeRiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
